bd45aa92b54f0dcc9dca4fe5521027e14b65c6e4
Saad DAHMANI <tahatatakor7@gmail.com>
2024-04-23 20:12:38 +0100

    add cos(x) data file

--- src/bin/src/cos_example.rs
+++ src/bin/src/cos_example.rs
@@ -9,11 +9,10 @@ use ndarray_csv::Array2Reader;
 use neuros::activations::Activations;
 use neuros::trainer::*;
 
-
 #[allow(dead_code)]
-pub fn ann_sin_test(){
+pub fn ann_cos_test(){
 
-    let path = "src/bin/src/data/sin_xy.csv";
+    let path = "src/bin/src/data/cos_x.csv";
     //--------------------------
     let data = read_csv_file(path);
 
@@ -21,17 +20,17 @@ pub fn ann_sin_test(){
         Err(eror) => println!("I can not read the given file because of :{:?}", eror),
         Ok(data)=> {
                         
-            // Split the data to take the first two columns as inputs and the third (last) column as output.
+            // Split the data to take the first column as input and the second (last) column as output.
             // records : inputs,
             // targets : outputs.
-            let (records, targets) = data.view().split_at(Axis(1), 2);
+            let (records, targets) = data.view().split_at(Axis(1), 1);
             
             ////Create a data set from (inputs, outputs) samples
             let dataset : Dataset<f64, f64, Ix2> = Dataset::new(records.to_owned(),targets.to_owned());
            
                     
             //split the dataset into (80% learning, 20% testing) 
-            let split_ratio : f32 = 0.9;            
+            let split_ratio : f32 = 0.8;            
             
             let (train_set, test_set) = dataset.split_with_ratio(split_ratio);
            
@@ -39,7 +38,7 @@ pub fn ann_sin_test(){
             let mut ann : Evonet = Evonet::empty();
             ann.add_layer(Layer::new(records.dim().1, Activations::Sigmoid));
             ann.add_layer(Layer::new(3, Activations::Sigmoid));
-            ann.add_layer(Layer::new(3, Activations::Sigmoid));
+            ann.add_layer(Layer::new(2, Activations::Sigmoid));
             ann.add_layer(Layer::new(targets.dim().1, Activations::Linear));
 
             let params : EoSettings = EoSettings::new(50, 1000, -10.0, 10.0, 2.0, 1.0, 0.5);
--- src/bin/src/main.rs
+++ src/bin/src/main.rs
@@ -25,12 +25,6 @@ fn main() {
 #[allow(dead_code)]
 fn ann_xor_test(){
 
-     // Give the ANN structure. 
-     let mut layers_struct : Vec<Layer> = Vec::new();
-     layers_struct.push(Layer::new(2, Activations::Sigmoid));
-     layers_struct.push(Layer::new(4, Activations::Sigmoid));
-     layers_struct.push(Layer::new(1, Activations::Linear));
-
     //Give input data samples
     let records = array![[0.,0.], [1.,0.], [0.,1.], [1.,1.]];
     
@@ -40,6 +34,13 @@ fn ann_xor_test(){
     //Create a data set from (inputs, outputs) samples
     let dataset : Dataset<f64, f64, Ix2> = Dataset::new(records, targets);
     
+     // Give the ANN structure. 
+     let mut layers_struct : Vec<Layer> = Vec::new();
+     layers_struct.push(Layer::new(2, Activations::Sigmoid));
+     layers_struct.push(Layer::new(4, Activations::Sigmoid));
+     layers_struct.push(Layer::new(1, Activations::Linear));
+
+
     // To use Growth optimizer (GO) as ANN trainer:
     //let params : trainer::GoSettings =  trainer::GoSettings::new(50,500, -5.0, 5.0);
     // You can use default parameters,
