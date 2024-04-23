00376d4ee598b0f0bd98c21dcad3678709c9e0cf
Saad DAHMANI <tahatatakor7@gmail.com>
2024-04-23 15:51:19 +0100

    update docs

--- src/bin/src/main.rs
+++ src/bin/src/main.rs
@@ -7,7 +7,7 @@ use ndarray::{array, Ix2};
 use linfa::dataset::Dataset;
 
 //--------------------------------------------------------------------------
-use neuros::{activations::Activations, trainer::{Evonet, Layer, TrainingAlgo, EoSettings, PsoSettings, GoSettings}};
+use neuros::{activations::Activations, trainer::{self, EoSettings, Evonet, Layer, TrainingAlgo}};
 //--------------------------------------------------------------------------
 fn main() {
     println!("Hello, NEUROS!");
@@ -34,20 +34,23 @@ fn ann_test_xor(){
     let dataset : Dataset<f64, f64, Ix2> = Dataset::new(records, targets);
     
     // To use Growth optimizer (GO) as ANN trainer:
-    let params : GoSettings = GoSettings::new(50,500, -5.0, 5.0);
+    //let params : trainer::GoSettings =  trainer::GoSettings::new(50,500, -5.0, 5.0);
+    // You can use default parameters,
     //let params : GoSettings =GoSettings::default();
     //let trainer : TrainingAlgo = TrainingAlgo::GO(params);
    
     // To use Equilibrium optimizer (EO) as ANN trainer:
     let params : EoSettings = EoSettings::new(50,500, -10.0, 10.0, 2.0, 1.0, 0.5);
+    // You can use default parameters,
     //let params : EoSettings = EoSettings::default();
     let trainer : TrainingAlgo = TrainingAlgo::EO(params);
    
     // To use Particle Swarm optimizer (PSO) as ANN trainer:
-    let params : PsoSettings = PsoSettings::new(50, 500, -10.0, 10.0, 2.0, 2.0);
-    let trainer : TrainingAlgo = TrainingAlgo::PSO(params);
-         
-    
+    // let params : trainer::PsoSettings = trainer::PsoSettings::new(50, 500, -10.0, 10.0, 2.0, 2.0);
+    // You can use default parameters,
+    // let params : trainer::PsoSettings = trainer::PsoSettings::default();
+    //let trainer : TrainingAlgo = TrainingAlgo::PSO(params);
+             
      //Create an artificial neural network using the given .
     let mut ann = Evonet::new(layers_struct);
 
