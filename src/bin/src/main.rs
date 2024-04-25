306f574975087e9ecada848682b4e32418d2e855
Saad DAHMANI <tahatatakor7@gmail.com>
2024-04-25 18:09:45 +0100

    split ann_xor_example.

--- src/bin/src/main.rs
+++ src/bin/src/main.rs
@@ -9,18 +9,19 @@ use linfa::dataset::Dataset;
 mod sin_example;
 mod cos_example;
 
-
 //------------------------------------------------------------------------------------------
 use neuros::{activations::Activations, trainer::{self, EoSettings, Evonet, Layer, TrainingAlgo}};
 
-use crate::{cos_example::ann_cos_test, sin_example::ann_sin_test};
+use crate::sin_example::ann_sin_test;
+//use crate::cos_example::ann_cos_test;
+
 //------------------------------------------------------------------------------------------
 fn main() {
     println!("Hello, NEUROS!");
     
     //ann_xor_test();
 
-    ann_sin_test();
+     ann_sin_test();
 
     //ann_cos_test();
 }
@@ -43,38 +44,62 @@ fn ann_xor_test(){
      layers_struct.push(Layer::new(4, Activations::Sigmoid));
      layers_struct.push(Layer::new(dataset.targets.dim().1, Activations::Linear)); // 1 neurons
 
-
-    // To use Growth optimizer (GO) as ANN trainer:
-    //let params : trainer::GoSettings =  trainer::GoSettings::new(50,500, -5.0, 5.0);
-    // You can use default parameters,
-    let params : trainer::GoSettings = trainer::GoSettings::default();
-    let trainer : TrainingAlgo = TrainingAlgo::GO(params);
-   
-    // To use Equilibrium optimizer (EO) as ANN trainer:
-    //let params : EoSettings = EoSettings::new(50,500, -10.0, 10.0, 2.0, 1.0, 0.5);
-    // You can use default parameters,
-    //let params : EoSettings = EoSettings::default();
-    //let trainer : TrainingAlgo = TrainingAlgo::EO(params);
-   
-    // To use Particle Swarm optimizer (PSO) as ANN trainer:
-    // let params : trainer::PsoSettings = trainer::PsoSettings::new(50, 500, -10.0, 10.0, 2.0, 2.0);
-    // You can use default parameters,
-    // let params : trainer::PsoSettings = trainer::PsoSettings::default();
-    //let trainer : TrainingAlgo = TrainingAlgo::PSO(params);
-             
-     //Create an artificial neural network using the given .
+      //Create an artificial neural network using the given .
     let mut ann = Evonet::new(layers_struct);
 
-    // You can use 'add_layer()' function to add layer. 
-    ann.add_layer(Layer::new(1, Activations::Linear));
+     // To use Equilibrium optimizer (EO) as ANN trainer:
+     let params : EoSettings = EoSettings::new(50,500, -5.0, 5.0, 2.0, 1.0, 0.5);
+     // You can use default parameters,
+     // let params : EoSettings = EoSettings::default();
+
+     println!("----------- Training ANN using Equilibrium Optimizer (EO) -----------");
+     
+     let trainer : TrainingAlgo = TrainingAlgo::EO(params);
+ 
+     // You can use 'add_layer()' function to add layer. 
+     ann.add_layer(Layer::new(1, Activations::Linear));
+ 
+    let train_result = ann.do_learning(&trainer, &dataset);
+ 
+    println!("EO - The ANN train results = {}", train_result.to_string());
+  
+    let sample = [0.0, 0.0];
+    let resp = ann.compute_output(&sample);   
+  
+    println!("EO - ANN response of :{:?} is {:?}", sample, resp);
+
+    //--------------------------------------------------------------------------------------------------------
+
+    println!("----------- Training ANN using Growth Optimizer (GO) -----------");
+
+    // To use Growth optimizer (GO) as ANN trainer:
+    let params : trainer::GoSettings =  trainer::GoSettings::new(50,500, -5.0, 5.0);
+     // You can use default parameters,
+     //let params : trainer::GoSettings = trainer::GoSettings::default();
+    let trainer : TrainingAlgo = TrainingAlgo::GO(params);
+   
+    let train_result = ann.do_learning(&trainer, &dataset);
+
+    println!("GO - The ANN train results = {}", train_result.to_string());
+     
+    let resp = ann.compute_output(&sample);   
+  
+    println!("GO - ANN response of :{:?} is {:?}", sample, resp);
+    //------------------------------------------------------------------------------------------------------------
+    
+    println!("----------- Training ANN using Paricle Swarm Optimizer (PSO) -----------");
+
+    // To use Particle Swarm optimizer (PSO) as ANN trainer:
+    let params : trainer::PsoSettings = trainer::PsoSettings::new(50, 500, -5.0, 5.0, 2.0, 2.0);
+    // You can use default parameters,
+    // let params : trainer::PsoSettings = trainer::PsoSettings::default();
+    let trainer : TrainingAlgo = TrainingAlgo::PSO(params);
 
     let train_result = ann.do_learning(&trainer, &dataset);
 
-    println!("The ANN train results = {}", train_result.to_string());
-
-    let sample = [0.0, 0.0];
+    println!("PSO - The ANN train results = {}", train_result.to_string());
+    
     let resp = ann.compute_output(&sample);   
-
-    println!("ANN response of :{:?} is {:?}", sample, resp);
- 
+  
+    println!("PSO - ANN response of :{:?} is {:?}", sample, resp);   
 }
